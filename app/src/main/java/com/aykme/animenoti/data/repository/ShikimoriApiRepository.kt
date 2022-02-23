672a8d1783a5b08920b7d5e43a82f4c429cef9eb
aykme <coffeewithoutlemon@gmail.com>
2022-02-24 01:06:23 +0700

    Refactoring

--- app/src/main/java/com/aykme/animenoti/data/repository/ShikimoriApiRepository.kt
+++ app/src/main/java/com/aykme/animenoti/data/repository/ShikimoriApiRepository.kt
@@ -79,12 +79,19 @@ class ShikimoriApiRepository(private val api: ShikimoriApi) : ApiRepository {
             } catch (e: Throwable) {
                 print(e.stackTrace)
                 Log.d(tag, "Api failure, trying again")
-                delay(2500)
+                delay(2575)
                 try {
                     apiCall.invoke()
                 } catch (e: Throwable) {
-                    Log.d(tag, "Api failure")
-                    throw NetworkErrorException("Api failure")
+                    print(e.stackTrace)
+                    Log.d(tag, "Api failure, trying again")
+                    delay(5575)
+                    try {
+                        apiCall.invoke()
+                    } catch (e: Throwable) {
+                        Log.d(tag, "Api failure")
+                        throw NetworkErrorException("Api failure")
+                    }
                 }
             }
         }
